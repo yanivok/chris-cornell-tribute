a0b74e27714ff33ea3bfe912a5f78fd00df65d1a
Yaniv Okev <yaniv@spectory.com>
2017-07-06 22:51:23 +0300

    refactoring component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Video from './Video.js';
 import Patephone from './Patephone.js';
+import videosArray from './videos.json';
 import './App.css';
 import './fonts/fonts.css';
 
@@ -8,13 +9,49 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentVideoIndex: 0,
+      currentVideo: null
     }
+    this.youtubeOptions = {
+      height: '100vh',
+      width: '100vw',
+      playerVars: { 
+        autoplay: 1,
+        wmode: 'transparent',
+        showinfo: 0,
+        controls: 0,
+        cc_load_policy: 0,
+        disablekb: 1,
+        iv_load_policy: 3,
+        modestbranding: 1,
+        playsinline: 0,
+        rel: 0
+      }
+    };
+  }
+
+  componentWillMount() {
+    this.onStartSong();
+  }
+
+  onStartSong = () => {
+    this.setState({currentVideo: videosArray[this.state.currentVideoIndex]});
+  }
+
+  onSongEnd = () => {
+    if (this.state.currentVideoIndex === videosArray.length - 1){ return; }
+    this.setState({currentVideoIndex: this.state.currentVideoIndex + 1}, ()=>{
+      this.onStartSong();
+    })
   }
 
   render() {
     return (
       <div className="main-container">
-        <Video/>
+        <Video opts={this.youtubeOptions} 
+               currentVideo={this.state.currentVideo}
+               onSongEnd={this.onSongEnd}
+               onStartSong={this.onStartSong} />
         <Patephone />
         <div className="video-title">
           <div className="band">audioslave</div>
--- src/Video.js
+++ src/Video.js
@@ -1,57 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import YouTube from 'react-youtube';
-import videosArray from './videos.json';
 
-class Video extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentVideoIndex: 0,
-      currentVideo: null
-    }
-  }
-
-  componentWillMount() {
-    this.onStartSong();
-  }
-
-  onStartSong = () => {
-    this.setState({currentVideo: videosArray[this.state.currentVideoIndex]});
-  }
-
-  onSongEnd = () => {
-    if (this.state.currentVideoIndex === videosArray.length - 1){ return; }
-    this.setState({currentVideoIndex: this.state.currentVideoIndex + 1}, ()=>{
-      this.onStartSong();
-    })
-  }
-  
-  render() {
-    const opts = {
-      height: '100vh',
-      width: '100vw',
-      playerVars: { 
-        autoplay: 1,
-        wmode: 'transparent',
-        showinfo: 0,
-        controls: 0,
-        cc_load_policy: 0,
-        disablekb: 1,
-        iv_load_policy: 3,
-        modestbranding: 1,
-        playsinline: 0,
-        rel: 0
-      }
-    };
-    return (
+export const Video = (props) => {
+  return(
       <YouTube
         className="video-iframe"
-        videoId={this.state.currentVideo.videoId}
-        opts={opts}
-        onEnd={this.onSongEnd}
+        videoId={props.currentVideo.videoId}
+        opts={props.opts}
+        onEnd={props.onSongEnd}
       />
-    );
-  }
+  )
 }
 
 export default Video;
